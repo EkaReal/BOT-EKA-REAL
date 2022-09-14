24e22a680dd197d7f05b7fcaa0022347eeaea1b8
EkaReal <107825416+EkaReal@users.noreply.github.com>
2022-09-14 15:24:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -43,26 +43,25 @@ global.sdc = 'https://youtube.com/channel/UCACHvReRmw2fxgMutPFCBWg' //Discord Mu
 global.snh = 'https://nhentai.net/g/365296/' //Make ini ana gausah di ganti.
 
 /*⫘⫘⫘⫘ALL PAYMENT⫘⫘⫘⫘⫘*/
-global.pdana = '088215689772' //Dana
-global.povo = '088215689772' //Ovo
-global.pgopay = '088215689772' //Gopay
-global.ppulsa = '088215689772' //Pulsa 
-global.ppulsa2 = '0895394988123' //Pulsa, Kalau nomor cuma 1, yg ini kasih tanda -
-global.psaweria = 'https://saweria.co/FangzBot' //Saweria
+global.pdana = 'pm' //Dana
+global.povo = 'pm' //Ovo
+global.pgopay = 'pm' //Gopay
+global.ppulsa = '085754568378' //Pulsa 
+global.ppulsa2 = '085754568378' //Pulsa, Kalau nomor cuma 1, yg ini kasih tanda 
 
 /*⫘⫘⫘⫘ GLOBAL NOMOR ⫘⫘⫘⫘⫘*/
-global.nomorbot = '62895394988123' //Nomor Bot mu
-global.nomorown = '6288215689772' //Nomormu
-global.namebot = 'Fangz BOT' //Nama bot mu
-global.nameown = 'Fangz Ganz' //Nama mu
+global.nomorbot = '6288704330448' //Nomor Bot mu
+global.nomorown = '6288704330448' //Nomormu
+global.namebot = 'Ekaa BOT' //Nama bot mu
+global.nameown = 'Eka Official' //Nama mu
 
 
 /*⫘⫘⫘⫘ STAFF ⫘⫘⫘⫘⫘*/
 global.owner = [
-  ['6288215689772'], //Owner1
-  ['6285335877178'], //Owner2
-  ['6288215689772'], //Owner3
-  ['6288215689772', 'FANGZ ! 🥶', true] //Ini buat kamu!
+  ['6288704330448'], //Owner1
+  ['6288704330448'], //Owner2
+  ['6288704330448'], //Owner3
+  ['6288704330448', 'Ekaa ! 🥶', true] //Ini buat kamu!
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
